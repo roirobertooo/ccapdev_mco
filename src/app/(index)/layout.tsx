557814f7dcfb539a6fdb740a0ec58efaf1fa94a0
Victor <roirobertooo@gmail.com>
2024-02-19 19:34:44 +0800

    Added title for page

--- src/app/(index)/layout.tsx
+++ src/app/(index)/layout.tsx
@@ -1,6 +1,11 @@
 import "@/app/ui/globals.css";
 
+import {Metadata} from "next";
 import Template from "../ui/template";
+
+export const metadata: Metadata = {
+  title: "Chow: Your Refined Guide to Campus Gastronomy"
+};
 
 export default function RootLayout({
                                        children,
