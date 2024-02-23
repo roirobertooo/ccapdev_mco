2d9d322b5e0024455bc6a2827b815052d3789973
Victor <roirobertooo@gmail.com>
2024-02-24 07:19:40 +0800

    loogin page done, forms functionality left. Added a search bar in the index page, functionality to be done once index page is complete. json file for data is created.

--- src/app/(pages)/(registration)/(user)/login/page.tsx
+++ src/app/(pages)/(registration)/(user)/login/page.tsx
@@ -1,9 +1,76 @@
-// TODO: Login Page
+// TODO: Login Page: Forms functionality
+import React from "react";
+
+import Image from "next/image";
 
 export default function Page() {
     return (
-        <div>
-            <h1>Login Page</h1>
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 -mt-28">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <Image className="mx-auto h-10 w-auto unselectable" src="/logo.png" width={40} height={40} alt="Logo" priority/>
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                    Log in to your account
+                </h2>
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form className="space-y-6" action="#" method="POST">
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                            Email address
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                autoComplete="email"
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brandBlue sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className="flex items-center justify-between">
+                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                Password
+                            </label>
+                            <div className="text-sm">
+                                <a href="#" className="font-semibold text-brandBlue hover:text-blue-600">
+                                    Forgot password?
+                                </a>
+                            </div>
+                        </div>
+                        <div className="mt-2">
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                autoComplete="current-password"
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brandBlue sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <button
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-brandBlue px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Login
+                        </button>
+                    </div>
+                </form>
+
+                <p className="mt-10 text-center text-sm text-gray-500">
+                    Not a member?{' '}
+                    <a href="/signup" className="font-semibold leading-6 text-brandBlue hover:text-blue-600">
+                        Sign up!
+                    </a>
+                </p>
+            </div>
         </div>
     );
 }
---
+++ src/app/lib/sample-data.js
@@ -0,0 +1,24 @@
+// TODO: Move this to a database on Phase 3.
+
+const user_accounts = [
+
+]
+
+const business_accounts = [
+
+]
+
+const restaurants = [
+
+]
+
+const reviews = [
+
+]
+
+module.exports = {
+    user_accounts,
+    business_accounts,
+    restaurants,
+    reviews,
+};
