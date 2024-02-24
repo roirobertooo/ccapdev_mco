7d0fb47e0de37f062ea1460408fcc0e4d666f77c
Victor <roirobertooo@gmail.com>
2024-02-24 13:29:02 +0800

    small tweaks in ui

--- src/app/(pages)/(registration)/(user)/login/page.tsx
+++ src/app/(pages)/(registration)/(user)/login/page.tsx
@@ -26,7 +26,7 @@ export default function Page() {
                                 name="username"
                                 type="text"
                                 required
-                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brandBlue sm:text-sm sm:leading-6"
+                                className="block w-full rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brandBlue sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
@@ -42,7 +42,7 @@ export default function Page() {
                                 type="password"
                                 autoComplete="current-password"
                                 required
-                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brandBlue sm:text-sm sm:leading-6"
+                                className="block w-full rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-brandBlue sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
@@ -60,7 +60,7 @@ export default function Page() {
                     <div>
                         <button
                             type="submit"
-                            className="flex w-full justify-center rounded-md bg-brandBlue px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            className="flex w-full justify-center rounded-full bg-brandBlue px-3 py-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 duration-200"
                         >
                             Login
                         </button>
