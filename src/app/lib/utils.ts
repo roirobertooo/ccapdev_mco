603b461dd00286003fef3e267d019b2c0e0e1640
Victor <roirobertooo@gmail.com>
2024-02-27 21:08:05 +0800

    updated utils function added the date formatter

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -11,11 +11,28 @@ export const useFetchData = <T>(fetchString: string): T | undefined => {
             setData(data);
         };
 
+        const params = new URLSearchParams(fetchString.split('?')[1]);
+        const collectionName = params.get('collectionName');
+
         fetchData().then(
-            () => console.log(`Fetched ${fetchString.split("/")[2]} data`),
-            (error) => console.error(`Error fetching ${fetchString.split("/")[2]} data`, error)
+            () => console.log(`Fetched ${collectionName} data`),
+            (error) => console.error(`Error fetching ${collectionName} data`, error)
         );
     }, [fetchString]);
 
     return data;
+};
+
+export const formatDateToLocal = (
+    dateStr: string,
+    locale: string = 'en-US',
+) => {
+    const date = new Date(dateStr);
+    const options: Intl.DateTimeFormatOptions = {
+        day: 'numeric',
+        month: 'short',
+        year: 'numeric',
+    };
+    const formatter = new Intl.DateTimeFormat(locale, options);
+    return formatter.format(date);
 };
