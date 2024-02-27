daf0c769e497e1173b912d530493acf7b86b8085
Victor <roirobertooo@gmail.com>
2024-02-27 19:59:34 +0800

    refactored restaurants listing for the new util fetch data function

--- src/app/ui/home/restaurants-listing.tsx
+++ src/app/ui/home/restaurants-listing.tsx
@@ -1,4 +1,6 @@
-import {useEffect, useState} from 'react';
+import React from "react";
+
+import {useFetchData} from "../../lib/utils";
 
 import {EmblaOptionsType} from 'embla-carousel';
 import EmblaCarousel from '../interactivity/carousel/carousel';
@@ -7,31 +9,21 @@ import '../interactivity/carousel/css/embla.css';
 import {Restaurant} from "../../lib/definitions";
 
 export default function RestaurantListing() {
-    const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch(`/api/restaurant/getAll?sortKey=name&sortOrder=asc`);
-            const data = await response.json();
-
-            setRestaurants(data);
-        };
-
-        fetchData().then(
-            () => console.log('Fetched all restaurants'),
-            (error) => console.error('Error fetching all restaurants', error)
-        );
-    }, []);
+    const fetchString = `/api/restaurant/getAll?sortKey=name&sortOrder=asc`;
+    const restaurants = useFetchData<Restaurant[]>(fetchString);
 
     const OPTIONS: EmblaOptionsType = {align: 'end', slidesToScroll: 'auto', loop: true};
 
     return (
-        <div className="mt-10">
-            <h2 className="font-bold text-3xl mb-7">
+        <div className="flex flex-col justify-center items-center mt-10 w-full">
+            <h2 className="justify-self-start font-bold text-3xl mb-7 w-2/3">
                 Restaurants
             </h2>
 
-            <EmblaCarousel slides={restaurants} options={OPTIONS}/>
+            {restaurants ?
+                <EmblaCarousel slides={restaurants} options={OPTIONS}/> :
+                <div className="italic">There are no available restaurants.</div>
+            }
         </div>
     );
 }
