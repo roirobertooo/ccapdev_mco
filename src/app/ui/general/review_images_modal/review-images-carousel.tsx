f3971ca95737ed734438fc8e9491f264ea41a404
Victor <roirobertooo@gmail.com>
2024-03-05 16:29:26 +0800

    in progress images carousel

---
+++ src/app/ui/general/review_images_modal/review-images-carousel.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {Review} from "../../../lib/definitions";
+
+function ReviewImagesCarousel({review}: { review: Review }) {
+    return (
+        //     implement carousel here...
+        <>
+            <div>Test</div>
+        </>
+    );
+}
+
+export default ReviewImagesCarousel;
---
+++ src/app/ui/general/review_images_modal/review-images-modal.tsx
@@ -0,0 +1,42 @@
+import {useState} from 'react';
+
+import {Dialog} from '@headlessui/react';
+import ReviewImagesCarousel from "./review-images-carousel";
+
+import {Review} from "../../../lib/definitions";
+
+function ReviewImagesModal({review}: { review: Review }) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const closeModal = () => {
+        setIsOpen(false);
+    }
+
+    const openModal = () => {
+        setIsOpen(true);
+    }
+
+    return (
+        <>
+            <button onClick={openModal} className="text-blue-700 font-medium hover:underline">
+                View Media
+            </button>
+
+            <Dialog open={isOpen} onClose={closeModal} className="fixed z-10 inset-0 overflow-y-auto">
+                <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                    <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"/>
+
+                    <span className="hidden sm:inline-block sm:align-middle sm:h-screen"
+                          aria-hidden="true">&#8203;</span>
+
+                    <div
+                        className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl">
+                        <ReviewImagesCarousel review={review}/>
+                    </div>
+                </div>
+            </Dialog>
+        </>
+    );
+}
+
+export default ReviewImagesModal;
