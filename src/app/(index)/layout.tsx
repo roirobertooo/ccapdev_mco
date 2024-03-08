9c7a7168b3819efd1aec2b1fdf86d7c9763953a5
Victor <roirobertooo@gmail.com>
2024-03-08 18:26:16 +0800

    clean up

--- src/app/(index)/layout.tsx
+++ src/app/(index)/layout.tsx
@@ -4,14 +4,17 @@ import {Metadata} from 'next';
 import '@/app/ui/globals.css';
 import Template from '@/app/ui/template';
 
-export const metadata: Metadata = {
+const metadata: Metadata = {
     title: "Chow: Your Refined Guide to Campus Gastronomy"
 };
 
-export default function RootLayout({children}: Readonly<{ children: React.ReactNode }>) {
+function RootLayout({children}: Readonly<{ children: React.ReactNode }>) {
     return (
         <Template>
             {children}
         </Template>
     );
 }
+
+export {metadata};
+export default RootLayout;
