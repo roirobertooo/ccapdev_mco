ea76439e48a451a101f8a467d9e5f83b753559f3
Victor <roirobertooo@gmail.com>
2024-03-08 18:14:40 +0800

    clean up

--- src/pages/api/get.ts
+++ src/pages/api/get.ts
@@ -6,11 +6,11 @@ import clientPromise from '@/app/lib/mongodb';
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const collectionName = (req.query.collectionName) as string || null;
 
-    const findKeys = (req.query.findKeys as string)?.split(',') || [];
-    const findValues = (req.query.findValues as string)?.split(',') || [];
+    const findKeys = (req.query.findKeys as string)?.split(",") || [];
+    const findValues = (req.query.findValues as string)?.split(",") || [];
 
-    const sortKeys = (req.query.sortKeys as string)?.split(',') || [];
-    const sortOrders = (req.query.sortOrders as string)?.split(',') || [];
+    const sortKeys = (req.query.sortKeys as string)?.split(",") || [];
+    const sortOrders = (req.query.sortOrders as string)?.split(",") || [];
 
     try {
         const client = await clientPromise;
@@ -20,12 +20,12 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
         if (collectionName && findKeys && findValues && findKeys.length === findValues.length) {
             const queryObject = findKeys.reduce((obj: Record<string, any>, key, index) => {
-                obj[key] = key === '_id' ? new ObjectId(findValues[index]) : findValues[index];
+                obj[key] = key === "_id" ? new ObjectId(findValues[index]) : findValues[index];
                 return obj;
             }, {});
 
             const sortObject = sortKeys.reduce((obj: Record<string, any>, key, index) => {
-                obj[key] = sortOrders[index] === 'desc' ? -1 : 1;
+                obj[key] = sortOrders[index] === "desc" ? -1 : 1;
                 return obj;
             }, {});
 
