75b2e3777879de1916f86f833584557d0c38946d
Victor <roirobertooo@gmail.com>
2024-03-08 17:59:25 +0800

    clean up

--- src/app/ui/template.tsx
+++ src/app/ui/template.tsx
@@ -1,23 +1,21 @@
-import {Inter} from "next/font/google";
+import React from 'react';
+import {Inter} from 'next/font/google';
 
-import "./globals.css";
-
-import Navbar from "./navbar/navbar";
-import Footer from "./footer/footer";
+import './globals.css';
+import Navbar from './navbar/navbar';
+import Footer from './footer/footer';
 
 const inter = Inter({subsets: ["latin"]});
 
-export default function Template({
-                                     children,
-                                 }: Readonly<{
-    children: React.ReactNode;
-}>) {
+export default function Template({children}: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en">
         <body className={inter.className}>
-            <Navbar/>
-            {children}
-            <Footer/>
+        <Navbar/>
+
+        {children}
+
+        <Footer/>
         </body>
         </html>
     );
