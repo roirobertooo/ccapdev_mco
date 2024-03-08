8c5a6c998e1f8b97234792021f52456261ab8390
Victor <roirobertooo@gmail.com>
2024-03-08 18:33:19 +0800

    clean up

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,26 +1,32 @@
-// TODO: Main page to be done by Roi.
 'use client';
+
 import React from 'react';
 
-import SearchForm from "@/app/ui/home/search-form";
-import RestaurantListing from "@/app/ui/home/restaurants-listing";
-import ReviewListing from "@/app/ui/home/reviews-listing/reviews-listing";
+import SearchForm from '@/app/ui/home/search-form';
+import RestaurantsListing from '@/app/ui/home/restaurants-listing';
+import ReviewsListing from '@/app/ui/home/reviews-listing/reviews-listing';
 
-export default function Home() {
+function Home() {
     return (
-        <main className="flex flex-col items-center justify-between">
-            <h1 className="text-4xl font-bold text-center my-5">Taste, Discover, Chow?</h1>
-            <h2 className="text-xl text-center my-2 mb-7">Find the best restaurants and food places in DLSU</h2>
+        <main className="flex flex-col justify-between items-center">
+            <h1 className="text-4xl font-bold text-center my-5">
+                Taste, Discover, Chow?
+            </h1>
+            <h2 className="text-xl text-center my-2 mb-7">
+                Find the best restaurants and food places in DLSU
+            </h2>
 
             <SearchForm/>
 
-            <RestaurantListing/>
+            <RestaurantsListing/>
 
-            <hr className="w-4/5 mt-16 border-[1px]"/>
+            <hr className="border-[1px] w-4/5 mt-16"/>
 
-            <ReviewListing/>
+            <ReviewsListing/>
 
             <div className="my-10"></div>
         </main>
     );
 }
+
+export default Home;
