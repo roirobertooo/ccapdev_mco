8c2de0aa42ff6607ba5971a3f261fdc2f921753d
Victor <roirobertooo@gmail.com>
2024-03-08 18:52:57 +0800

    clean up

---
+++ src/app/(pages)/(main)/about_us/page.tsx
@@ -0,0 +1,14 @@
+// TODO: About Us Page (Check specs for more details)
+
+function Page() {
+    return (
+        // Start here.
+        <div className="w-2/3 mx-auto mt-10">
+            <h1 className="text-3xl font-bold mb-10">
+                About Us
+            </h1>
+        </div>
+    );
+}
+
+export default Page;
