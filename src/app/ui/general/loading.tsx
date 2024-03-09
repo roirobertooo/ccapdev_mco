4357f241c2623e3835603b77bd58c1f1d9ef6442
Victor <roirobertooo@gmail.com>
2024-03-09 16:10:30 +0800

    clean up + loading ui

---
+++ src/app/ui/general/loading.tsx
@@ -0,0 +1,9 @@
+function Loading() {
+    return (
+        <div className="flex flex-row items-center justify-center h-[70vh]">
+            <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-blue-600"/>
+        </div>
+    );
+}
+
+export default Loading;
