be90d3c5ef2bdd3992b45659e911829820a60796
Victor <roirobertooo@gmail.com>
2024-03-09 16:43:08 +0800

    clean up

--- src/app/(pages)/(main)/restaurants/page.tsx
+++ src/app/(pages)/(main)/restaurants/page.tsx
@@ -1,9 +1,21 @@
-// TODO: Restaurants Page
+import {Metadata} from 'next';
+
+import RestaurantsListing from '@/app/ui/main/restaurants/restaurants-listing';
+
+const metadata: Metadata = {
+    title: "Restaurants Listing"
+};
 
 export default function Page() {
     return (
-        <div>
-            <h1>Restaurants Page</h1>
+        <div className="w-2/3 mx-auto mt-10">
+            <h1 className="font-bold text-3xl mb-10">
+                All Restaurants
+            </h1>
+
+            <RestaurantsListing/>
         </div>
     );
 }
+
+export {metadata};
