2e18296e6b9030e1a461838c19b6399b00dc55da
Victor <roirobertooo@gmail.com>
2024-03-10 13:29:35 +0800

    clean up

---
+++ src/app/ui/index/restaurants-listing.tsx
@@ -0,0 +1,31 @@
+import {EmblaOptionsType} from 'embla-carousel';
+
+import {useFetchData} from '@/app/lib/utils';
+import {Restaurant} from '@/applib/definitions';
+
+import Loading from '@/app/ui/loading';
+import '@/app/ui/components/interactivity/carousel/css/embla.css';
+import EmblaCarousel from '@/app/ui/components/interactivity/carousel/carousel';
+
+function RestaurantsListing() {
+    const fetchString = "/api/get?collectionName=restaurants&sortKeys=name&sortOrders=asc";
+    const [restaurants, error] = useFetchData<Restaurant[]>(fetchString);
+
+    const OPTIONS: EmblaOptionsType = {align: "end", slidesToScroll: "auto", loop: true};
+
+    return (
+        <div className="mt-10">
+            <h2 className={`font-bold text-3xl mb-7 ${restaurants ? "" : " -ml-[536px]"}`}>
+                Restaurants
+            </h2>
+
+            {restaurants ?
+                <EmblaCarousel slides={restaurants} options={OPTIONS}/>
+                :
+                <Loading/>
+            }
+        </div>
+    );
+}
+
+export default RestaurantsListing;
