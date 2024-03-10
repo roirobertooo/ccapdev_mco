f610b67ee7e963f5f46ea6ac31c4c5eb0e059733
Victor <roirobertooo@gmail.com>
2024-03-10 13:12:19 +0800

    clean up

--- src/app/ui/components/interactivity/dropdown-item.tsx
+++ src/app/ui/components/interactivity/dropdown-item.tsx
@@ -13,7 +13,9 @@ const DropdownItem: React.FC<DropdownItemProps> = ({label, setSelectedItem, setI
             setIsOpen(false);
         }}
            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-           role="menuitem">{label}</a>
+           role="menuitem">
+            {label}
+        </a>
     );
 }
 
