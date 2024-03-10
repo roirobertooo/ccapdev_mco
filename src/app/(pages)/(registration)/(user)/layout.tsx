03ed52e9de6dc2c47231edff638bb72c53b5f272
Victor <roirobertooo@gmail.com>
2024-03-10 10:56:33 +0800

    clean up

--- src/app/(pages)/(registration)/(user)/layout.tsx
+++ src/app/(pages)/(registration)/(user)/layout.tsx
@@ -1,19 +1,18 @@
-import {Inter} from "next/font/google";
+import React from 'react';
+import {Inter} from 'next/font/google';
 
-import "@/app/ui/globals.css";
+import '@/app/ui/globals.css';
 
 const inter = Inter({subsets: ["latin"]});
 
-export default function Layout({
-                                   children,
-                               }: Readonly<{
-    children: React.ReactNode;
-}>) {
+function Layout({children}: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en" className="h-full bg-white">
-        <body className="{inter.className} h-full">
-        {children}
+        <body className={`${inter.className} h-full`}>
+            {children}
         </body>
         </html>
     );
 }
+
+export default Layout;
