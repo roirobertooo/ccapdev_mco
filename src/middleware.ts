f49ae7bfd90f7e6a6208d3027c5327762411279c
Victor <roirobertooo@gmail.com>
2024-03-11 21:42:04 +0800

    added auth

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import type {NextRequest} from 'next/server';
+import NextAuth from 'next-auth';
+
+import {authConfig} from './auth.config';
+
+NextAuth(authConfig).auth;
+
+function middleware(request: NextRequest) {
+    const currentUser = request.cookies.get("currentUser")?.value;
+
+    console.log("middleware", currentUser); // TODO: Remove this line
+
+    if (currentUser && request.nextUrl.pathname.startsWith("/login")) {
+        return Response.redirect(new URL("/", request.url));
+    }
+}
+
+const config = {
+    matcher: ['/((?!api|_next/static|_next/image|images|favicon.ico).*)']
+};
+
+export {middleware, config};
+export default NextAuth;
