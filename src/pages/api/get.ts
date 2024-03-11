2faf6529c82c76920bbe63459d58db98b7323365
Victor <roirobertooo@gmail.com>
2024-03-11 23:38:21 +0800

    added auth

--- src/pages/api/get.ts
+++ src/pages/api/get.ts
@@ -12,7 +12,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const sortKeys = (req.query.sortKeys as string)?.split(",") || [];
     const sortOrders = (req.query.sortOrders as string)?.split(",") || [];
 
-    if (findValues[findKeys.indexOf("_id")] === "null" ||
+    if (findValues[findKeys.indexOf("_id")] === "null" || findValues[findKeys.indexOf("_id")] === "undefined" ||
         findKeys.length !== findValues.length || sortKeys.length !== sortOrders.length) {
         return;
     }
