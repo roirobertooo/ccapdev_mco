7314cf716bb72c44f181b32a00c4bce71ef69e00
Victor <roirobertooo@gmail.com>
2024-03-11 23:42:38 +0800

    added profile pages

---
+++ src/app/(pages)/(main)/users/[user]/page.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import React from 'react';
+import {notFound} from 'next/navigation';
+
+import {useFetchData} from '@/app/lib/utils';
+import {UserAccount, Review} from '@/app/lib/definitions';
+
+import Loading from '@/app/ui/loading';
+import Header from '@/app/ui/main/users/user_page/header';
+import ReviewEntry from '@/app/ui/main/restaurants/restaurant_page/review';
+
+function Page({params}: { params: { user: string } }) {
+    const userFetchString = `/api/get?collectionName=user_accounts&findKeys=_id&findValues=${params.user}`;
+    const [userData, userError] = useFetchData<UserAccount[]>(userFetchString);
+    const user = userData ? userData[0] : null;
+
+    if (userData !== undefined && userData !== null && userData.length === 0) {
+        notFound();
+    }
+
+    const reviewFetchString = `/api/get?collectionName=reviews&findKeys=user_id&findValues=${params.user}&sortKeys=date&sortOrders=desc`;
+    const [reviewsData, reviewsError] = useFetchData<Review>(reviewFetchString);
+
+    const establishmentFetchString = `/api/get?collectionName=reviews&findKeys=restaurant_id&findValues=${user && user.restaurant_owned}&sortKeys=date&sortOrders=desc`;
+    const [establishmentData, establishmentError] = useFetchData<Review[]>(establishmentFetchString);
+
+    return (
+        !user ?
+            <Loading/>
+            :
+            <div className="flex flex-row w-2/3 mx-auto mt-20 gap-40">
+                <div className="w-1/2 mx-auto">
+                    <Header user={user}/>
+
+                    {user.is_business ?
+                        <div className="-mt-5 rounded-md border-gray-500">
+                            <p className="text-xl font-bold mb-5">
+                                Establishment Reviews
+                            </p>
+
+                            {establishmentData && establishmentData.length === 0 ?
+                                <div className="w-full">
+                                    <p className="text-center w-full text-md font-medium italic border-[1px] p-4 rounded-md border-gray-500">
+                                        No reviews yet.
+                                    </p>
+                                </div>
+                                :
+                                establishmentData?.map(review => (
+                                    <ReviewEntry key={establishmentData._id} review={review}/>
+                                ))
+                            }
+                        </div>
+                        : <></>
+                    }
+                </div>
+                <div className="flex flex-col items-center w-1/2 mx-auto pt-5">
+                    {reviewsData && reviewsData.length === 0 ?
+                        <div className="w-2/3">
+                            <p className="text-center w-full text-md font-medium italic mb-5 border-[1px] p-4 rounded-md border-gray-500">
+                                No reviews yet.
+                            </p>
+                        </div>
+                        : <></>
+                    }
+
+                    {reviewsData?.map(review => (
+                        <ReviewEntry key={reviewsData._id} review={review}/>
+                    ))}
+                </div>
+            </div>
+    );
+}
+
+export default Page;
---
+++ src/app/ui/main/users/user_page/header.tsx
@@ -0,0 +1,73 @@
+import {useState} from 'react';
+import Link from 'next/link';
+
+import {useFetchData} from '@/app/lib/utils';
+import {UserAccount, Restaurant} from '@/app/lib/definitions';
+
+function Header({user}: { user: UserAccount }) {
+    const fetchString = `/api/get?collectionName=restaurants&findKeys=_id&findValues=${user.restaurant_owned}`;
+    const [restaurantData, error] = useFetchData<Restaurant[]>(fetchString);
+    const restaurant = restaurantData ? restaurantData[0] : null;
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    return (
+        <div className="flex flex-col">
+            <div className="flex flex-row items-center">
+                <img src={user.avatar_url} alt="avatar"
+                     className="w-20 h-20 rounded-full border-2 border-gray-400 unselectable"/>
+
+                <div className="ml-4">
+                    <p className="flex flex-row items-center gap-3">
+                        <span className="text-2xl font-bold">{user.name}</span>
+                        {user.is_business && restaurant &&
+                            <div className="flex flex-row items-center">
+                                <Link href={`/restaurants/${restaurant._id}`}
+                                      onMouseEnter={() => setIsModalOpen(true)}
+                                      onMouseLeave={() => setIsModalOpen(false)}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         strokeWidth={1.5}
+                                         stroke="currentColor" className="w-6 h-6 cursor-pointer">
+                                        <path strokeLinecap="round" strokeLinejoin="round"
+                                              d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z"/>
+                                    </svg>
+                                </Link>
+                                <div className="ml-3">
+                                    {isModalOpen && (
+                                        <div
+                                            className="flex flex-row items-center gap-1 z-10 border absolute bg-white p-1.5 rounded">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                 strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                                <path strokeLinecap="round" strokeLinejoin="round"
+                                                      d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"/>
+                                            </svg>
+                                            <div className="unselectable">
+                                                This user owns <span className="font-medium">{restaurant.name}</span>.
+                                            </div>
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
+                        }
+                    </p>
+                    <p className="mt-1 text-lg">@{user.username}</p>
+                </div>
+            </div>
+
+            <div className="flex justify-between mt-7">
+                <div className="w-full">
+                    <div className="border-[1px] rounded-md border-gray-500 p-4 h-fit mb-20">
+                        <p className="text-xl font-bold mb-2">
+                            Description
+                        </p>
+                        <p>
+                            {user.description}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
