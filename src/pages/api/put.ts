5d2f4a107f45851714173fca60d19d7efff31373
Victor <roirobertooo@gmail.com>
2024-03-14 23:12:41 +0800

    edit/delete review todo

--- src/pages/api/put.ts
+++ src/pages/api/put.ts
@@ -1,4 +1,5 @@
 import {NextApiRequest, NextApiResponse} from 'next';
+import {InsertOneResult, ObjectID} from 'mongodb';
 
 import clientPromise from '@/app/lib/mongodb';
 
@@ -16,7 +17,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         const client = await clientPromise;
         const db = client.db();
 
-        let documents: any[] = [];
+        let documents: InsertOneResult<Document> | null = null
 
         if (collectionName && putKeys && putValues && putKeys.length === putValues.length) {
             let insert = putKeys.reduce((obj: Record<string, any>, key, index) => {
@@ -61,6 +62,30 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
             documents = await db
                 .collection(collectionName)
                 .insertOne(insert);
+
+            if (collectionName === "reviews") {
+                const restaurant_id = insert["restaurant_id"];
+                const review_id = documents.insertedId.toString();
+
+                try {
+                    const reviews = await db.collection("reviews").find({restaurant_id: restaurant_id}).toArray();
+
+                    const totalRating = reviews.reduce((total, review) => total + review.rating, 0);
+                    let averageRating = totalRating / reviews.length;
+                    averageRating = Number(averageRating.toFixed(2));
+
+                    const result = await db.collection("restaurants").updateOne(
+                        {_id: new ObjectID(restaurant_id)},
+                        {
+                            $set: {review_average: averageRating},
+                            $inc: {review_count: 1},
+                            $push: {reviews: review_id}
+                        }
+                    );
+                } catch (e) {
+                    console.error(e);
+                }
+            }
         }
 
         res.json(documents);
