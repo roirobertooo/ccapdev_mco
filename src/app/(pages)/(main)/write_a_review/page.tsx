d7112942312c4386bf7658cb79c584376804cb82
Victor <roirobertooo@gmail.com>
2024-03-14 20:43:41 +0800

    write review

---
+++ src/app/(pages)/(main)/write_a_review/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import Form from '@/app/ui/main/write_a_review/form';
+
+function Page() {
+    return (
+        <div className="w-2/3 mx-auto mt-10">
+            <h1 className="font-bold text-2xl mb-10">
+                What&#39;s on your mind?
+            </h1>
+
+            <Form/>
+        </div>
+    );
+}
+
+export default Page;
