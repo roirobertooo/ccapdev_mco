0224d506f81f23dafb06ce949cb772a4179ac788
Victor <roirobertooo@gmail.com>
2024-03-14 20:32:46 +0800

    write review

--- src/app/(pages)/(main)/about_us/page.tsx
+++ src/app/(pages)/(main)/about_us/page.tsx
@@ -1,6 +1,7 @@
 // TODO: "About Us" Page (Check specs for more details)
 
 import {Metadata} from 'next';
+import {notFound} from 'next/navigation';
 
 const metadata: Metadata = {
     title: "About Us"
@@ -9,11 +10,7 @@ const metadata: Metadata = {
 function Page() {
     return (
         // Start here.
-        <div className="w-2/3 mx-auto mt-10">
-            <h1 className="text-3xl font-bold mb-10">
-                About Us
-            </h1>
-        </div>
+        notFound()
     );
 }
 
