ca4aedef1a92fcdf08eb57064499b27245eb9a1d
Victor <roirobertooo@gmail.com>
2024-03-14 19:00:35 +0800

    refactor for generalized signup validation

--- src/app/ui/signup/avatar-preview.tsx
+++ src/app/ui/signup/avatar-preview.tsx
@@ -40,7 +40,7 @@ const AvatarPreview: React.FC<AvatarPreviewProps> = ({onAvatarChange, isRequired
     return (
         <div>
             <input
-                className="block w-full px-6 py-3 text-black bg-white border border-gray-200 appearance-none rounded-xl placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
+                className="block px-6 py-3 text-black bg-white border border-gray-200 appearance-none rounded-xl placeholder:text-gray-400 focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
                 type="file"
                 id="avatar"
                 name="avatar"
