974eeb87a81f9e8bb25cbba49036149d1e694b3b
Victor <roirobertooo@gmail.com>
2024-03-16 14:02:27 +0800

    adding review in user account as well

--- src/app/ui/main/write_a_review/form.tsx
+++ src/app/ui/main/write_a_review/form.tsx
@@ -48,14 +48,7 @@ function Form({userId}: { userId: string }) {
         //     }
         // }
 
-        let reviewId;
-
-        try {
-            reviewId = await putData(`/api/put?collectionName=reviews&putKeys=user_id,restaurant_id,rating,date,review_title,review_body&putValues=${userId},${restaurant},${rating},${formattedDate},${title},${body}`);
-            console.log(reviewId);
-        } catch (error) {
-            console.error(error);
-        }
+        let reviewId = await putData(`/api/put?collectionName=reviews&putKeys=user_id,restaurant_id,rating,date,review_title,review_body&putValues=${userId},${restaurant},${rating},${formattedDate},${title},${body}`);
 
         if (user && reviewId) {
             await putData(`/api/post?collectionName=user_accounts&findKeys=_id&findValues=${userId}&updateKeys=review_count,reviews&updateValues=${user?.review_count + 1},${reviewId}`)
